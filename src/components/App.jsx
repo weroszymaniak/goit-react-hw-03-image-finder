f65b4118345928f91b2d600616ee60e1408488ba
weroszymaniak <weronika.szymaniak@hotmail.com>
2023-06-22 18:08:36 +0200

    added api.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Searchbar from './Searchbar/Searchbar';
+import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
+import ImageGallery from './ImageGallery/ImageGallery';
 
 class App extends Component {
   onSubmit = () => console.log('well');
 
   render() {
-    return <Searchbar onSubmit={this.onSubmit}></Searchbar>;
+    return (
+      <div>
+        <Searchbar onSubmit={this.onSubmit}></Searchbar>
+        <ImageGallery></ImageGallery>;
+      </div>
+    );
   }
 }
 
